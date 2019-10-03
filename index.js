442183544817f10f10a1ab6796052a721fbd625a
dptalks <dptalks@dpt.world>
2019-10-03 09:23:11 +0000

    initial commit. github, take over.

---
+++ index.js
@@ -0,0 +1,230 @@
+var express = require("express")
+var app = express()
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+var sqlite3 = require('sqlite3').verbose()
+const DBSOURCE = "feedback.db"
+const db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message)
+      throw err
+	}
+});
+
+var HTTP_PORT = 8011 
+
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+	next();
+});
+
+
+app.listen(HTTP_PORT, () => {
+	console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+
+app.get('/list', (req, res) => {
+	var output = '';
+	db.all("SELECT * FROM feedback", [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+		rows.forEach((row) => {
+			console.log(row.id);
+			output += `${row.id}:${row.q1}:${row.q2}:${row.q3}:${row.q4}:${row.freetext}\n`;
+		});
+		res.header("Content-Type", "text/plain");
+		res.send(output);
+	});
+});
+
+//app.post("/save", bodyparser, (req, res, next) => {
+app.post("/save", (req, res) => {
+	console.log(JSON.stringify(req.body));
+	if('a1' in req.body) {
+		db.run(`
+			INSERT INTO feedback VALUES (
+				null,
+				'${req.body.a1}',
+				'${req.body.a2}',
+				'${req.body.a3}',
+				'${req.body.a4}',
+				'${req.body.t}'
+			)
+		`);
+	}
+	res.end(JSON.stringify({"thank":"you"}));
+});
+app.get("/", (req, res) => {
+
+	res.send(`<!DOCTYPE html>
+	<html>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<head>
+        <title>Digital Peace Talks Feedback Form</title>
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+        <style>
+		body {
+			color: #fff;
+			background: #0071bc;
+		}
+		.rating {
+			direction: rtl;
+			font-size: 20px;
+			unicode-bidi: bidi-override;
+			padding: 10px 30px;
+			display: inline-block;
+		}
+		.rating input {
+			opacity: 0;
+			position: relative;
+			left: -15px;
+			z-index: 2;
+			cursor: pointer;
+		}
+		.rating span.star:before {
+			color: #777777;
+			content: "\uf006";
+		}
+		.rating span.star {
+			display: inline-block;
+			font-family: FontAwesome;
+			font-style: normal;
+			font-weight: normal;
+			position: relative;
+			z-index: 1;
+		}
+		.rating span {
+			margin-left: -15px;
+		}
+		.rating span.star:before {
+			color: #777777;
+			content: \uf006;
+		}
+		.rating input:hover + span.star:before,
+		.rating input:hover + span.star ~ span.star:before,
+		.rating input:checked + span.star:before,
+		.rating input:checked + span.star ~ span.star:before {
+			color: #ffd100;
+			content: "\uf005";
+		}
+		.selected-rating{
+			color: #ffd100;
+			font-weight: bold;
+		}
+        </style>
+	</head>
+	<body>
+	<div>
+        <form id="feedback">
+		<p><h2>Please rate the Digital Peace Talks project in these aspects:</h2></p>
+		<ul>
+		<li>	
+			How do you like the idea of the Digital Peace talks?
+			<br>
+			<span class="rating">
+			<input type="radio" name="rating1" value="5"><span class="star"></span>
+			<input type="radio" name="rating1" value="4"><span class="star"></span>
+    			<input type="radio" name="rating1" value="3"><span class="star"></span>
+			<input type="radio" name="rating1" value="2"><span class="star"></span>
+			<input type="radio" name="rating1" value="1"><span class="star"></span>
+			</span>
+		</li>
+		<li>
+			How do you like the movement of the 3D camera? (1-5 Sterne)
+			<br>
+			<span class="rating">
+			<input type="radio" name="rating2" value="5"><span class="star"></span>
+			<input type="radio" name="rating2" value="4"><span class="star"></span>
+    			<input type="radio" name="rating2" value="3"><span class="star"></span>
+			<input type="radio" name="rating2" value="2"><span class="star"></span>
+			<input type="radio" name="rating2" value="1"><span class="star"></span>
+			</span>
+		</li>
+		<li>
+			How do you like the chat module? (1-5 Sterne)
+			<br>
+			<span class="rating">
+			<input type="radio" name="rating3" value="5"><span class="star"></span>
+			<input type="radio" name="rating3" value="4"><span class="star"></span>
+    			<input type="radio" name="rating3" value="3"><span class="star"></span>
+			<input type="radio" name="rating3" value="2"><span class="star"></span>
+			<input type="radio" name="rating3" value="1"><span class="star"></span>
+			</span>
+		</li>
+		<li>
+			How do you like the login process? (1-5 Sterne)
+			<br>
+			<span class="rating">
+			<input type="radio" name="rating4" value="5"><span class="star"></span>
+			<input type="radio" name="rating4" value="4"><span class="star"></span>
+    			<input type="radio" name="rating4" value="3"><span class="star"></span>
+			<input type="radio" name="rating4" value="2"><span class="star"></span>
+			<input type="radio" name="rating4" value="1"><span class="star"></span>
+			</span>
+		</li>
+		</ul>
+		Feel free to express your Digital Peace Talks experience in your own words:<br>
+		<textarea id="freetext" name="freetext" rows="5" cols="63"></textarea>
+		<br>
+		<input type="submit" value="Send">
+		<span id="rating-result"></span>
+	</form>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript">
+	document.addEventListener("DOMContentLoaded", function(event) {
+		jQuery('#feedback').on('submit', function(event) {
+			event.preventDefault();
+			event.stopImmediatePropagation();
+			var a1 = jQuery('[name="rating1"]:checked').val();
+			var a2 = jQuery('[name="rating2"]:checked').val();
+			var a3 = jQuery('[name="rating3"]:checked').val();
+			var a4 = jQuery('[name="rating4"]:checked').val();
+			var t = jQuery('[name="freetext"]').val();
+			console.log({a1: a1, a2: a2, a3: a3, a4: a4, t: t});
+			try {
+			jQuery.ajax({ 
+				url: '/save',
+				type: 'POST',
+				cache: false, 
+				data: JSON.stringify({a1: a1, a2: a2, a3: a3, a4: a4, t: t}),
+				success: function(data){
+					alert('Thank You!');
+				},
+				error: function(jqXHR, status, err) {
+					alert('text status '+status+', err '+err);
+				},
+				dataType: 'json',
+				contentType: "application/json",
+			});
+			} catch(err) {
+				console.log('err: '+err);
+			}
+		});
+	});
+	</script>
+	</body>
+	</html>
+	`);
+	res.status(200);
+	console.log('bla');
+});
+
+app.use(function(req, res){
+	res.status(404);
+});
